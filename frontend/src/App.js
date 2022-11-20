81068525f3576d4f4a60658affc73fc2a496cf37
YOON-CC <whdbscks77@gmail.com>
2022-11-21 03:00:24 +0900

    홈 화면 css 수정 및 카테고리 about 작성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,50 @@
+import React, { useState } from "react";
 import './App.css';
-import Home from './Home'
 import Choice from './Choice'
+import About from './About'
 
 function App() {
-  //
 
+  //카테고리 변경시 페이지 렌더링
+
+  //화면 렌더링
+  const [page, setPage] = useState(1) 
   //리턴 부분
   return (
-    <div className='App_body'>
+    <body className='App_body'>
 
-      <div className="home_div">
-        <Home></Home>
+      {/* 상단 제목(메인 제목, 카테고리)*/}
+      <header className="home_header">
+        {/* CHAN 제목*/}
+        <div className="home">
+                <h1 className="home_page_title">CHAN</h1>
+            </div>
+
+            {/* 카테고리*/}
+            <div className="category">
+                <h1 className="category_1" onClick={() => setPage(1)}>HOME</h1>
+                <h1 className="space"></h1>
+                <h1 className="category_2" onClick={() => setPage(2)}>ABOUT</h1>
+                <h1 className="space"></h1>
+                <h1 className="category_3">MYPAGE</h1>
+                <h1 className="space"></h1>
+                <h1 className="category_3">LOGIN</h1>
+            </div>
+      </header>
+
+      {/*렌더링 화면들*/}
+      <div className="home_rendering">
+        {
+          page === 1
+          ? <Choice></Choice>
+          : ( page === 2 
+              ? <About></About>
+              : <p>첫번째 조건이 참이 아니고 두번째 조건도 참이 아니면 실행됩니다.</p> 
+            )
+        }
       </div>
 
-      <div className="home_div">
-        <Choice></Choice>
-      </div>
-
-    </div>
+    </body>
   );
 }
 
--- frontend/src/Choice.js
+++ frontend/src/Choice.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useState } from "react";
 import './Choice.css'
 
 function Choice() { 
+    
     return (
         <body>
-            {/* 화면표출*/}
+            {/*화면표출*/}
             <div className="show">
                 <div className="big_circle"></div>
-                <div className="circle_1"></div>
-                <div className="circle_2"></div>
-                <div className="circle_3"></div>
-                <div className="circle_4"></div>
-                <div className="circle_5"></div>
+
+                {/*서클1*/}
+                <div className="circle_1">
+                    <img class="circle_1_img"src="img/icon_1.png"></img>
+                </div>
+                {/*줌!*/}
+                <div className="zoom"></div>
+                <div className="ME">
+                    제가 누구인지 소개하는 페이지입니다.<br/>
+                </div>
+
+                {/*서클2*/}
+                <div className="circle_2">
+                    <img class="circle_2_img"src="img/icon_2.png"></img>
+                </div>
+                {/*줌!*/}
+                <div className="zoom"></div>
+
+                {/*서클3*/}
+                <div className="circle_3">
+                    <img class="circle_3_img"src="img/icon_3.png"></img>
+                </div>
+                {/*줌!*/}
+                <div className="zoom"></div>
+
+                {/*서클4*/}
+                <div className="circle_4">
+                    <img class="circle_4_img"src="img/icon_4.png"></img>
+                </div>
+                {/*줌!*/}
+                <div className="zoom"></div>
+
+                {/*서클5*/}
+                <div className="circle_5">
+                    <img class="circle_5_img"src="img/icon_5.png"></img>
+                </div>
+                {/*줌!*/}
+                <div className="zoom"></div>
+
             </div>
+
 
         </body>
 
