77709be3e69909dc034922460dd6db08feeb0ed3
YOON-CC <whdbscks77@gmail.com>
2022-11-20 14:16:50 +0900

    홈페이지 시작화면 완료(제목, 카테고리, 기본틀)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home'
+import Choice from './Choice'
 
 function App() {
+  //
+
+  //리턴 부분
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App_body'>
+
+      <div className="home_div">
+        <Home></Home>
+      </div>
+
+      <div className="home_div">
+        <Choice></Choice>
+      </div>
+
     </div>
   );
 }
---
+++ frontend/src/Choice.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Choice.css'
+
+function Choice() { 
+    return (
+        <body>
+            {/* 화면표출*/}
+            <div className="show">
+                <div className="big_circle"></div>
+                <div className="circle_1"></div>
+                <div className="circle_2"></div>
+                <div className="circle_3"></div>
+                <div className="circle_4"></div>
+                <div className="circle_5"></div>
+            </div>
+
+        </body>
+
+    );
+}
+
+export default Choice;
