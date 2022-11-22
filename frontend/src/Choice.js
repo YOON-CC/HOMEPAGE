92dd135e24ef50772c2a319cd3d509c0c228be90
YOON-CC <whdbscks77@gmail.com>
2022-11-23 01:29:36 +0900

    Home 화면 ui 완성(드래그 엔 드롭은 구현 전)

--- frontend/src/Choice.js
+++ frontend/src/Choice.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import './Choice.css'
 
-function Choice() { 
+
+function Choice() {
     
     return (
         <body>
@@ -10,18 +11,18 @@ function Choice() {
                 <div className="big_circle"></div>
 
                 {/*서클1*/}
-                <div className="circle_1">
-                    <img class="circle_1_img"src="img/icon_1.png"></img>
+                <div className="circle_1" draggable="true">
+                    <img class="circle_1_img" src="img/icon_1.png" ></img>
+                    <p className="text_1">Who am I</p>
                 </div>
                 {/*줌!*/}
-                <div className="zoom"></div>
-                <div className="ME">
-                    제가 누구인지 소개하는 페이지입니다.<br/>
+                <div className="zoom"> 
                 </div>
 
                 {/*서클2*/}
                 <div className="circle_2">
                     <img class="circle_2_img"src="img/icon_2.png"></img>
+                    <p className="text_2">My experiences</p>
                 </div>
                 {/*줌!*/}
                 <div className="zoom"></div>
@@ -29,6 +30,7 @@ function Choice() {
                 {/*서클3*/}
                 <div className="circle_3">
                     <img class="circle_3_img"src="img/icon_3.png"></img>
+                    <p className="text_3">Etc</p>
                 </div>
                 {/*줌!*/}
                 <div className="zoom"></div>
@@ -36,6 +38,7 @@ function Choice() {
                 {/*서클4*/}
                 <div className="circle_4">
                     <img class="circle_4_img"src="img/icon_4.png"></img>
+                    <p className="text_4">My memory</p>
                 </div>
                 {/*줌!*/}
                 <div className="zoom"></div>
@@ -43,6 +46,7 @@ function Choice() {
                 {/*서클5*/}
                 <div className="circle_5">
                     <img class="circle_5_img"src="img/icon_5.png"></img>
+                    <p className="text_5">My study</p>
                 </div>
                 {/*줌!*/}
                 <div className="zoom"></div>
